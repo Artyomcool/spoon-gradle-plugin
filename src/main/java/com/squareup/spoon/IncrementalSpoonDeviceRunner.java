561b114faedcc89fcc64af68f04cccd7b1819d8f
Artyomcool <artem.drozdov@corp.mail.ru>
2014-12-30 19:39:44 +0300

    add: screenshots at end of each test

--- src/main/java/com/squareup/spoon/IncrementalSpoonDeviceRunner.java
+++ src/main/java/com/squareup/spoon/IncrementalSpoonDeviceRunner.java
@@ -1,12 +1,10 @@
 package com.squareup.spoon;
 
-import com.android.ddmlib.FileListingService;
-import com.android.ddmlib.IDevice;
-import com.android.ddmlib.InstallException;
-import com.android.ddmlib.SyncService;
+import com.android.ddmlib.*;
 import com.android.ddmlib.logcat.LogCatMessage;
 import com.android.ddmlib.testrunner.IRemoteAndroidTestRunner;
 import com.android.ddmlib.testrunner.RemoteAndroidTestRunner;
+import com.android.ddmlib.testrunner.TestIdentifier;
 import com.google.common.base.Strings;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
@@ -14,7 +12,10 @@ import com.squareup.spoon.adapters.TestIdentifierAdapter;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -119,7 +120,7 @@ public class IncrementalSpoonDeviceRunner {
 	/** Execute instrumentation on the target device and return a result summary.
 	 * @param className
 	 * @param methodName*/
-	public boolean run(String className, String methodName) {
+	public boolean run(final String className, final String methodName) {
 		String testPackage = instrumentationInfo.getInstrumentationPackage();
 		String testRunner = instrumentationInfo.getTestRunnerClass();
 		TestIdentifierAdapter testIdentifierAdapter = TestIdentifierAdapter.fromTestRunner(testRunner);
@@ -151,17 +152,53 @@ public class IncrementalSpoonDeviceRunner {
 				runner.setTestSize(testSize);
 			}
 			runner.run(
-					new SpoonTestListener(result, debug, TestIdentifierAdapter.JUNIT),
-					new XmlTestRunListener(junitReport)
-			);
-		} catch (Exception e) {
+                    new SpoonTestListener(result, debug, TestIdentifierAdapter.JUNIT),
+                    new XmlTestRunListener(junitReport) {
+                        @Override
+                        public void testEnded(TestIdentifier test, Map<String, String> testMetrics) {
+                            takeScreenshot(className, methodName);
+                            super.testEnded(test, testMetrics);
+                        }
+                    }
+            );
+        } catch (Exception e) {
 			result.addException(e);
 			return false;
 		}
 		return true;
 	}
 
-	public DeviceResult finish() {
+    private void takeScreenshot(String className, String methodName) {
+        BufferedImage image = null;
+        try {
+            image = getScreenshot();
+            imageDir.mkdirs();
+            File output = getScreenshotFile(className, methodName);
+            ImageIO.write(image, "png", output);
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (AdbCommandRejectedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BufferedImage getScreenshot() throws TimeoutException, AdbCommandRejectedException, IOException {
+        RawImage screenshot = device.getScreenshot();
+        BufferedImage image = new BufferedImage(screenshot.width, screenshot.height, BufferedImage.TYPE_INT_ARGB);
+        int index=0;
+        int bytesPerPixel=screenshot.bpp >> 3;
+        for (int y=0; y < screenshot.height; y++) {
+            for (int x=0; x < screenshot.width; x++) {
+                image.setRGB(x,y,screenshot.getARGB(index) | 0xff000000);
+                index+=bytesPerPixel;
+            }
+        }
+        return image;
+    }
+
+    public DeviceResult finish() {
 
 		String appPackage = instrumentationInfo.getApplicationPackage();
 
@@ -171,6 +208,10 @@ public class IncrementalSpoonDeviceRunner {
 			DeviceTestResult.Builder builder = result.getMethodResultBuilder(entry.getKey());
 			if (builder != null) {
 				builder.setLog(entry.getValue());
+                String className = entry.getKey().getClassName();
+                String methodName = entry.getKey().getMethodName();
+                File screenshot = getScreenshotFile(className, methodName);
+                builder.addScreenshot(screenshot);
 			}
 		}
 
@@ -184,62 +225,8 @@ public class IncrementalSpoonDeviceRunner {
 			String localDirName = work.getAbsolutePath();
 			logInfo(localDirName);
 			final String devicePath = "/data/data/" + appPackage + "/" + dirName;
-			FileListingService.FileEntry deviceDir = obtainDirectoryFileEntry(devicePath);
 			logDebug(debug, "Pulling screenshots from [%s] %s", serial, devicePath);
 
-			device.getSyncService()
-					.pull(new FileListingService.FileEntry[] {deviceDir}, localDirName, SyncService.getNullProgressMonitor());
-
-			File screenshotDir = new File(work, dirName);
-			if (screenshotDir.exists()) {
-				imageDir.mkdirs();
-
-				// Move all children of the screenshot directory into the image folder.
-				File[] classNameDirs = screenshotDir.listFiles();
-				if (classNameDirs != null) {
-					Multimap<DeviceTest, File> testScreenshots = ArrayListMultimap.create();
-					for (File classNameDir : classNameDirs) {
-						String className = classNameDir.getName();
-						File destDir = new File(imageDir, className);
-						FileUtils.copyDirectory(classNameDir, destDir);
-
-						// Get a sorted list of all screenshots from the device run.
-						List<File> screenshots = new ArrayList<File>(
-								FileUtils.listFiles(destDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE));
-						Collections.sort(screenshots);
-
-						// Iterate over each screenshot and associate it with its corresponding method result.
-						for (File screenshot : screenshots) {
-							String methodName = screenshot.getParentFile().getName();
-
-							DeviceTest testIdentifier = new DeviceTest(className, methodName);
-							DeviceTestResult.Builder builder = result.getMethodResultBuilder(testIdentifier);
-							if (builder != null) {
-								builder.addScreenshot(screenshot);
-								testScreenshots.put(testIdentifier, screenshot);
-							} else {
-								logError("Unable to find test for %s", testIdentifier);
-							}
-						}
-					}
-
-					// Don't generate animations if the switch is present
-					if (!noAnimations) {
-						// Make animated GIFs for all the tests which have screenshots.
-						for (DeviceTest deviceTest : testScreenshots.keySet()) {
-							List<File> screenshots = new ArrayList<File>(testScreenshots.get(deviceTest));
-							if (screenshots.size() == 1) {
-								continue; // Do not make an animated GIF if there is only one screenshot.
-							}
-							File animatedGif = getFile(imageDir, deviceTest.getClassName(),
-									deviceTest.getMethodName() + ".gif");
-							createAnimatedGif(screenshots, animatedGif);
-							result.getMethodResultBuilder(deviceTest).setAnimatedGif(animatedGif);
-						}
-					}
-				}
-				FileUtils.deleteDirectory(screenshotDir);
-			}
 		} catch (Exception e) {
 			result.addException(e);
 		}
@@ -247,7 +234,11 @@ public class IncrementalSpoonDeviceRunner {
 		return result.endTests().build();
 	}
 
-	private static File getFile(File dir, String... parts) {
+    private File getScreenshotFile(String className, String methodName) {
+        return new File(imageDir, "screen_" + className + "-" + methodName + ".png");
+    }
+
+    private static File getFile(File dir, String... parts) {
 		File file = dir;
 		for (String part : parts) {
 			file = new File(file, part);
